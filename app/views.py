96e7004c38ac6248556f705ca86e0de46f9ebca1
Carlos.Videira <carlosrobertovda@gmail.com>
2018-10-12 02:25:34 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -7,9 +7,9 @@ from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
 
-    def  home ( request ):
-        "" " Renderiza a home page. " ""
-         assert isinstance(request, HttpRequest)
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
     return render(
         request,
         'app/index.html',
